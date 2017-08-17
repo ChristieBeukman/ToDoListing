4a9a2143bf8121ac343441dd1e753e6899920463
Christie Beukman <me.mcbeukman@gmail.com>
2017-08-17 15:55:00 +0200

    Interface to CategoryItem and Suppliier created and added

---
+++ Framework.ToDo/Framework.ToDo/Services/IDataAccessSupplier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Framework.ToDo.Model;
+
+namespace Framework.ToDo.Services
+{
+    public interface IDataAccessSupplier
+    {
+        ObservableCollection<Suppliier> GetSuppliers();
+        void CreateSupplier(Suppliier Sup);
+        void DeleteSupplier(Suppliier Sup);
+        void UpdateSupplier(Suppliier NewSup, Suppliier OldSup);
+    }
+}
